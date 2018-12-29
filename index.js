17a6c2a10f69c12cf753d037de0c4fae6a2f3710
charlie  lee <ctlee9096@gmail.com>
2018-12-29 03:27:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ class Passenger {
     this.name = name
   store.passengers.push(this)
   }
+  trips(){
+    return store.trips.filter(
+      function(trip) {
+        return trip.driverId === this.id;
+      }.bind(this)
+    );
+  }
 }
 
 class Trip {
