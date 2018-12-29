1802c6266eecbd144ab2242e6cf8853fbdae2bf5
charlie  lee <ctlee9096@gmail.com>
2018-12-29 01:23:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
+//counter variables to count drivers
 let driverID = 0
+//counter variable to count passengers
 let passengerID = 0
+//counter variable to count trips
 let tripID = 0
+// initialize store with key of items and users that each point to an empty array
 let store = {drivers: [], passengers: [], trips: []}
+
 class Driver {
   constructor(name) {
+      // increment driverId, then assign the driverId as the instance's id
     this.id = ++driverID
     this.name = name
+     // insert in the driver to the store
     store.drivers.push(this)
   }
+  //how would we implement a method on our driver object that finds the associated trips?
   trips() {
-    return store.trips.filter(
-      function(trip) {
-        return trip.driverId === this.id
-      }.bind(this)
+    return store.trips.filter(//go through store and search through all trips
+      function(trip) {//go through store and search through all trips
+        return trip.driverId === this.id //to find a trip's driverID that "strictly equates"
+        //with this instance's driverID
+      }.bind(this)  //use bind method on trip's instance to this driver instance
     )
   }
   passengers() {
+    //return this Driver instance trips by map method
+    //with passed in argument of
+    //callback anonymous function object with trip argument
     return this.trips().map(function(trip) {
-      return trip.passenger()
+      //what is this?
+      debugger;
+      //{The map() method creates a new array with
+      //the results of calling a function for every array element.
+      //The map() method calls the provided function once for each element in an array, in order.
+      return trip.passenger()    //that returns trip's passenger value
     })
   }
 }
 class Passenger {
+  //can create a Passenger with a name
   constructor(name) {
-    this.id = ++passengerID
-    this.name = name
-    store.passengers.push(this)
+    this.id = ++passengerID//adds a numerical id to each passenger
+    //adds a unique id to each passenger
+    this.name = name //assigns this Passenger name attribute value to name argument
+    store.passengers.push(this)//adds this passenger instance object to the store's collection of passengers
   }
   trips() {
     return store.trips.filter(
       function(trip) {
         return trip.passengerId === this.id
-      }.bind(this)
+      }.bind(this)  //use bind method on trip's instance to this driver instance
     )
   }
   drivers() {
-    return this.trips().map(function(trip) {
-      return trip.driver()
+    return this.trips().map(function(trip)
+    //{The map() method creates a new array with
+    //the results of calling a function for every array element.
+    //The map() method calls the provided function once for each element in an array, in order.
+      return trip.driver() //return driver of the trip instance
     })
   }
-}
+
 
 class Trip {
   constructor(driver, passenger) {
@@ -54,9 +76,11 @@ class Trip {
   }
 
   driver() {
+    //now let's write a method trip.driver() such that the driver associated with the trip is returned.
     return store.drivers.find(
       function(driver) {
-        return driver.id === this.driverId
+        return driver.id === this.driverId//to find a stores driverID that "strictly equates"
+        //with this Trip's instance's driverID
       }.bind(this)
     )
   }
@@ -68,5 +92,4 @@ class Trip {
       }.bind(this)
     )
   }
-
 }
